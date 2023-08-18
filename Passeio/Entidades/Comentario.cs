bf6ffb6eae30a23c36ea351bca5563541d357e87
Alexandra Natacha <Alexandra Natacha@DESKTOP-BQFHBDA>
2023-08-17 22:06:48 -0300

    Adicionar arquivos de projeto.

---
+++ Passeio/Entidades/Comentario.cs
@@ -0,0 +1,20 @@
+﻿namespace Passeio.Entidades
+{
+    public class Comentario
+    {
+        public Comentario(string descricao, string usuarioAutor)
+        {
+            Descricao = descricao;
+            UsuarioAutor = usuarioAutor;
+            DataDeCriacao = DateTime.Now;
+        }
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+        public string UsuarioAutor{ get; set; }
+        public DateTime DataDeCriacao { get; set; }
+
+        public void CriarComentario() {}
+        public List<Comentario> ListarComentarios() { }
+        public void DeletarComentario(Guid id) { }
+    }
+}
---
+++ Passeio/Entidades/Local.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Passeio.Entidades
+{
+    public class Local
+    {
+        public Local(string titulo, string descricao, string localização, byte imagem, string usuarioCriador, List<Comentario> comentarios)
+        {
+            Titulo = titulo;
+            Descricao = descricao;
+            Localização = localização;
+            Imagem = imagem;
+            UsuarioCriador = usuarioCriador;
+            Comentarios = comentarios;
+            DataDeCriacao = DateTime.Now;
+        }
+
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Localização { get; set; }
+        public Byte Imagem { get; set; }
+        public string UsuarioCriador { get; set; }
+        public List<Comentario> Comentarios { get; set; }
+        public DateTime DataDeCriacao { get; set; }
+        public DateTime DataDeAtualizacao { get; set; }
+
+        public Local VisualizarLocal(Guid id){}
+        public void CriarLocal() {}
+        public Local EditarLugar(Guid id) {}
+        public void DeletarLocal(Guid id) {}
+    }
+}
